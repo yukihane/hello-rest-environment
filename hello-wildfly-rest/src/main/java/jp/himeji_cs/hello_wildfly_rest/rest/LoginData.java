2006442aa0801c163fa07137f7cc4947243655ef
yukihane <yukihane.feather@gmail.com>
2017-05-15 06:50:36 +0900

    JAAS設定

---
+++ hello-wildfly-rest/src/main/java/jp/himeji_cs/hello_wildfly_rest/rest/LoginData.java
@@ -0,0 +1,38 @@
+package jp.himeji_cs.hello_wildfly_rest.rest;
+
+public class LoginData {
+
+    private String name;
+    private String email;
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name
+     *            the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email
+     *            the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+}
--- hello-wildfly-rest/src/main/java/jp/himeji_cs/hello_wildfly_rest/rest/MemberResource.java
+++ hello-wildfly-rest/src/main/java/jp/himeji_cs/hello_wildfly_rest/rest/MemberResource.java
@@ -1,17 +1,16 @@
 package jp.himeji_cs.hello_wildfly_rest.rest;
 
+import io.swagger.annotations.Api;
 import java.util.List;
-
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-
 import jp.himeji_cs.hello_wildfly_rest.data.MemberRepository;
 import jp.himeji_cs.hello_wildfly_rest.model.Member;
 import jp.himeji_cs.hello_wildfly_rest.service.MemberRegistration;
@@ -19,15 +18,20 @@ import jp.himeji_cs.hello_wildfly_rest.service.MemberRegistration;
 @RequestScoped
 @Path("/members")
 @Produces(MediaType.APPLICATION_JSON)
+@Api
 public class MemberResource {
 
     @Inject
     private MemberRegistration regist;
-    
+
     @Inject
     private MemberRepository rep;
 
+    @Inject
+    private HttpServletRequest request;
+
     @POST
+    @Path("/init")
     public boolean init() {
         Member member = new Member();
         member.setName("yukihane");
@@ -42,9 +46,24 @@ public class MemberResource {
             return false;
         }
     }
-    
+
     @GET
     public List<Member> members() {
         return rep.findAllOrderedByName();
+    }
+
+    @POST
+    @Path("/login")
+    public boolean login(LoginData data) {
+
+        try {
+            request.login(data.getName(), data.getEmail());
+            System.out.println(request.getUserPrincipal());
+            return true;
+        } catch (ServletException e) {
+            e.printStackTrace();
+            return false;
+        }
+
     }
 }
